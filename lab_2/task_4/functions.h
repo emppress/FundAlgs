a2dccce72ee34ddb9f981fc95bf3593660eb5ac6
emppress <rozhkovvanya.2005@yandex.ru>
2024-09-29 18:03:37 +0300

    task2_4 готов кроме примеров

---
+++ lab_2/task_4/functions.h
@@ -0,0 +1,32 @@
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+#include "stdio.h"
+#include <stdlib.h>
+#include <limits.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <math.h>
+#include <string.h>
+
+#define CAPACITY 100
+
+typedef enum Status
+{
+    SUCCESS,
+    INPUT_ERROR,
+    MEMORY_ERROR,
+    OVERFLOW_ERROR
+} status;
+
+typedef struct Point
+{
+    double x;
+    double y;
+} point;
+
+int is_convex(int count_corners, ...);
+status find_polynom(double *res, double x, int n, ...);
+status is_kaprekar(int *result, int count_numbers, int base, ...);
+
+#endif
---
+++ lab_2/task_4/main.c
@@ -0,0 +1,35 @@
+#include "functions.h"
+
+int main()
+{
+    point p1, p2, p3, p4, p5;
+    p1 = (point){-4., 1.};
+    p2 = (point){-2., 4.};
+    p3 = (point){2., 5.};
+    p4 = (point){5., 2.};
+    p5 = (point){0., 1.44444};
+    printf("%s\n", is_convex(5, p5, p4, p3, p2, p1) ? "YES" : "NO");
+
+    double result;
+    switch (find_polynom(&result, 2., 2, 2., 3., 1.))
+    {
+    case INPUT_ERROR:
+        printf("Input error\n");
+        return INPUT_ERROR;
+        break;
+    case OVERFLOW_ERROR:
+        printf("Overflow error\n");
+        return OVERFLOW_ERROR;
+        break;
+
+    case SUCCESS:
+        printf("%lf\n", result);
+        break;
+    }
+
+    if (is_kaprekar(NULL, 1, 16, "1") != SUCCESS)
+    {
+        printf("Input error\n");
+        return INPUT_ERROR;
+    }
+}

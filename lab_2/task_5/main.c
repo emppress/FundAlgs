039219b4810635d1e5d8f1bbd88f07195972a29b
emppress <rozhkovvanya.2005@yandex.ru>
2024-10-04 20:05:19 +0300

    fix

--- lab_2/task_5/main.c
+++ lab_2/task_5/main.c
@@ -3,78 +3,78 @@
 int main(int argc, char *argv[])
 {
     char buff[BUFSIZ];
-
+    int count;
     printf("\t\tTest 1\n");
-    oversprintf(buff, "%d %d %Ro %.2f", 3888, 3, 4, 3.22);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%d %d %Ro %.2f", 3888, 3, 4, 3.22);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 2\n");
-    oversprintf(buff, "strokaaaaa %Ro", 6);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %Ro", 6);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 3\n");
-    oversprintf(buff, "strokaaaaa %Roover", 567);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %Roover", 567);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 4\n");
-    oversprintf(buff, "strokaaaaa %Roover", 100000000);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %Roover", 100000000);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 5\n");
-    oversprintf(buff, "strokaaaaa pupupu %Ro", 5);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa pupupu %Ro", 5);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 6\n");
-    oversprintf(buff, "strokaaaaa %", 100000000);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %", 100000000);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 7\n");
-    oversprintf(buff, "strokaaaaa %Cv", 123, 2);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %Cv", 123, 2);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 8\n");
-    oversprintf(buff, "strokaaaaa %Cv", -123, 16);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %Cv", -123, 16);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 9\n");
-    oversprintf(buff, "strokaaaaa %CV", 123, 2);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %CV", 123, 2);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 10\n");
-    oversprintf(buff, "strokaaaaa %CV", -123, 16);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %CV", -123, 16);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 11\n");
-    oversprintf(buff, "strokaaaaa %to", "1111011", 2);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %to", "1111011", 2);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 12\n");
-    oversprintf(buff, "strokaaaaa %to", "-7b", 16);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %to", "-7b", 16);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 13\n");
-    oversprintf(buff, "strokaaaaa %TO", "-7B", 10);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "strokaaaaa %TO", "-7B", 10);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 14\n");
-    oversprintf(buff, "%mi", -123);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%mi", -123);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 15\n");
-    oversprintf(buff, "%mu", 123);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%mu", 123);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 16\n");
-    oversprintf(buff, "%md", 0.1);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%md", 0.1);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 17\n");
-    oversprintf(buff, "%mf", (float)10034.34);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%mf", (float)10034.34);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     printf("\t\tTest 18\n");
-    oversprintf(buff, "%Zr", 10);
-    printf("string: '%s'\n\n", buff);
+    count = oversprintf(buff, "%Zr", 10);
+    printf("string: '%s'; count symbols: %d\n\n", buff, count);
 
     FILE *output = fopen("test.txt", "w");
     if (output == NULL)

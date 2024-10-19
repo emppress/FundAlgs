34e9c9f50a56e8e25dcec4468c4e49044b103329
emppress <rozhkovvanya.2005@yandex.ru>
2024-10-19 21:12:58 +0300

    fix realpath (realpath в си11 нет, а он работает wtf???) ✝✝✝✝

--- lab_3/task_3/functions.c
+++ lab_3/task_3/functions.c
@@ -4,12 +4,10 @@ status validate_input(int argc, char **argv)
 {
     if (argc != 4 || (argv[2][0] != '/' && argv[2][0] != '-') || argv[2][2] != '\0')
         return INPUT_ERROR;
-    char *idx_1 = strrchr(argv[1], '/');
-    char *idx_2 = strrchr(argv[3], '/');
-    idx_1 = (idx_1) ? (idx_1 + 1) : argv[1];
-    idx_2 = (idx_2) ? (idx_2 + 1) : argv[3];
-
-    if (!strcmp(idx_1, idx_2))
+    char full_path_1[BUFSIZ], full_path_2[BUFSIZ];
+    if (!realpath(argv[1], full_path_1) || !realpath(argv[3], full_path_2))
+        return INPUT_ERROR;
+    if (!strcmp(full_path_1, full_path_2))
         return INPUT_ERROR;
     return SUCCESS;
 }

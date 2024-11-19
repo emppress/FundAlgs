243efe84deeeebdf2a6692f5dcfb19fc568f7a25
emppress <rozhkovvanya.2005@yandex.ru>
2024-11-19 21:46:27 +0300

    task 5.4 done

---
+++ lab_5/task_4/main.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cmath>
+
+class complex
+{
+private:
+    double _real, _im;
+
+public:
+    complex(double real = 0, double im = 0) : _real(real), _im(im) {};
+
+    double module() const { return sqrt(_real * _real + _im * _im); }
+
+    void add(const complex &value)
+    {
+        _real += value._real;
+        _im += value._im;
+    }
+
+    void sub(const complex &value)
+    {
+        _real -= value._real;
+        _im -= value._im;
+    }
+
+    void mult(const complex &value)
+    {
+        _real = _real * value._real - _im * value._im;
+        _im = _real * value._im + _im * value._real;
+    }
+
+    void div(const complex &value)
+    {
+        double divider = value._im * value._im + value._real * value._real;
+        if (divider == 0)
+            throw std::overflow_error("Divide by zero exception");
+        _real = (_real * value._real + _im * value._im) / divider;
+        _im = (_im * value._real - _real * value._im) / divider;
+    }
+
+    double argument() const
+    {
+        if (_real == 0)
+            return 90.;
+        return atan(_im / _real) * 180 / M_PI;
+    }
+
+    double im_accessor() const { return _im; }
+    double real_accerssor() const { return _real; }
+    void info() const
+    {
+        std::cout << this->real_accerssor() << " + " << this->im_accessor() << "i; Argument = "
+                  << this->argument() << "°; Module = " << this->module() << std::endl;
+    }
+};
+
+int main()
+{
+    complex a(9.21, 1.21);
+    complex b(0, 0);
+    std::cout << "a = ";
+    a.info();
+    std::cout << "b = ";
+    b.info();
+
+    std::cout << "\nTest div:\n"
+              << std::endl;
+    try
+    {
+        a.div(b);
+    }
+    catch (std::overflow_error &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    a.info();
+    b = complex(1.22, 1421.22);
+    std::cout << "new b = ";
+    b.info();
+
+    try
+    {
+        a.div(b);
+        std::cout << "a = ";
+        a.info();
+    }
+    catch (std::overflow_error &e)
+    {
+        std::cout << e.what() << std::endl;
+    }
+
+    a = complex(100, 100);
+    b = complex(10, 15);
+    std::cout << "\nnew a = ";
+    a.info();
+    std::cout << "new b = ";
+    b.info();
+
+    std::cout << "\nTest add:"
+              << std::endl;
+    a.add(b);
+    std::cout << "a = ";
+    a.info();
+    std::cout << "\nTest sub:"
+              << std::endl;
+    a.sub(b);
+    std::cout << "a = ";
+    a.info();
+    std::cout << "\nTest mult:"
+              << std::endl;
+    a.mult(b);
+    std::cout << "a = ";
+    a.info();
+    return 0;
+}
